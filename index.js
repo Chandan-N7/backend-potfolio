b056db5e5d835986b7a03f019cb0959e818d94f0
Chandan-N7 <nagarkoti.dev7@gmail.com>
2024-08-30 18:02:56 +0530

    update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors")
+const helmet = require("helmet")
 const cookiesParser = require('cookie-parser')
 const mongoose = require('mongoose');
 const authRoutes = require("./routes/AuthRoute.js");
@@ -14,11 +15,17 @@ const app = express()
 const port = process.env.PORT || 3000;
 const db = process.env.DB;
 
-app.use(cors({
-    origin:process.env.ORIGIN,
-    methods:["GET", "POST", "PUT", "PATCH", "DELETE"],
-    credentials:true,
-}))
+app.use(cors());
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+app.use(helmet({
+    helmet.crossOriginResourcePolicy: false,
+  }));
 
 const uploadsDir = path.join(__dirname, 'uploads');
 if (!fs.existsSync(uploadsDir)) {
