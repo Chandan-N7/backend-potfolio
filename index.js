480560ed030eb3673dc08519d51be223af9128df
Chandan-N7 <nagarkoti.dev7@gmail.com>
2024-08-30 19:21:25 +0530

    update index.js

--- index.js
+++ index.js
@@ -15,11 +15,18 @@ const app = express()
 const port = process.env.PORT || 3000;
 const db = process.env.DB;
 
-app.use(cors({
-    origin:process.env.ORIGIN,
-    methods:["GET", "POST", "PUT", "PATCH", "DELETE"],
-    credentials:true,
-}))
+app.use(cors());
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
+app.use(helmet({
+    crossOriginResourcePolicy: false,
+  }));
 
 const uploadsDir = path.join(__dirname, 'uploads');
 if (!fs.existsSync(uploadsDir)) {
